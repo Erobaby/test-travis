1488ca239b96c0d4f41dcda684612cab8c9814f0
erobaby <wwb0111@gmail.com>
2017-09-19 22:47:27 +0800

    Initial commit

---
+++ src/main/java/me/erobaby/testtravis/TestTravisApplication.java
@@ -0,0 +1,12 @@
+package me.erobaby.testtravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTravisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTravisApplication.class, args);
+	}
+}
---
+++ src/test/java/me/erobaby/testtravis/TestTravisApplicationTests.java
@@ -0,0 +1,16 @@
+package me.erobaby.testtravis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestTravisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
